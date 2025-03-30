6b6de8338bcafacad3d4b91ba118b32293247d99
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2025-03-30 03:17:58 +0100

    Lab 1-3

---
+++ Week2/Lab03.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+np.set_printoptions(precision=2)
+from lab_utils_multiclass_TF import *
+import logging
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
+tf.autograph.set_verbosity(0)
+
+# make 4-class dataset for classification
+classes = 4
+m = 100
+centers = [[-5, 2], [-2, -2], [1, 2], [5, -2]]
+std = 1.0
+X_train, y_train = make_blobs(n_samples=m, centers=centers, cluster_std=std,random_state=30)
+
+tf.random.set_seed(1234)  # applied to achieve consistent results
+model = Sequential(
+    [
+        Dense(2, activation = 'relu',   name = "L1"),
+        Dense(4, activation = 'linear', name = "L2")
+    ]
+)
+model.compile(
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=tf.keras.optimizers.Adam(0.01),
+)
+
+model.fit(
+    X_train,y_train,
+    epochs=200
+)
